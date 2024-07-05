eeadf61ed4618a14f21d514de7210301d8f40d17
Matthias Neid <matthias@aternos.org>
2024-07-05 14:35:22 +0200

    add size to structure

--- src/Structure.php
+++ src/Structure.php
@@ -4,6 +4,14 @@ class Structure
 {
     protected array $elements = [];
 
+    public function __construct(
+        protected int $sizeX,
+        protected int $sizeY,
+        protected int $sizeZ
+    )
+    {
+    }
+
     public function addElement(Element $element): void
     {
         $this->elements[] = $element;
